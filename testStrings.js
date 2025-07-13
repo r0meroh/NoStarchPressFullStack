057013de1bf5b4ebe3ea76c0afc1cd2ff38bd89b
r0meroh <romeroh862@gmail.com>
2025-07-12 20:58:09 -0500

    build: practice with arrow functions

---
+++ testStrings.js
@@ -0,0 +1,9 @@
+let a = 1;
+let b = 2;
+
+let string = `${a} + ${b} = ${a + b}`;
+console.log(string);
+
+let printStringArrow = (int1, int2) => `${int1} + ${int2} = ${int1 + int2}`;
+
+console.log(printStringArrow(a, b)); 
