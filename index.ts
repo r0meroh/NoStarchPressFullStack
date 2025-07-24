6e96e5f494cb1ba555a617f6acc875c874732059
r0meroh <romeroh862@gmail.com>
2025-07-23 21:55:35 -0500

    Build: Added endpoint for HTML file in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { routeHello, routeAPINames, routeWeather } from "./routes.js";
 import express, {Request, Response} from "express";
 
+import path from "path";
+
 const server = express();
 const port = 3000;
 
@@ -26,6 +28,12 @@ server.get("/api/weather/:zipcode", function (req: Request, res: Response): void
     res.send(response);
     }
 );
+
+server.get("/component/weather", function (req: Request, res: Response): void {
+    const filePath = path.join(process.cwd(), "public", "weather.html");
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(filePath);
+});
 
 server.listen(port, function (): void {
     console.log('Listening on port ' + port);
